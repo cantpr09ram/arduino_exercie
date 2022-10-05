03430abd111cd9c716739ea533a6e00dacec6dfe
bo_chain <53370227+bochainwu@users.noreply.github.com>
2022-10-05 11:04:36 +0800

    Update sketch_sep28b.ino
    it's a homework

--- sketch_sep28b.ino
+++ sketch_sep28b.ino
@@ -1,18 +1,16 @@
+int brightness = 0;
+int fadeamount = 64;
 void setup() {
   // put your setup code here, to run once:
-  int i;
-  for(int i = 4; i < 8; i++){
-    pinMode(i,OUTPUT);
-  }
-
+  pinMode(3,OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  for(int i = 4; i < 8; i++){
-    for(int j = 0; j < 257; j += 10){
-      analogWrite(i, j);
-      delay(50);
-    }
+  analogWrite(3, brightness);
+  brightness += fadeamount;
+  if(fadeamount == 0 || fadeamount == 192){
+    fadeamount = -fadeamount;
   }
+  delay(10);
 }
