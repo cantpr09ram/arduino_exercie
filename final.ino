664d1f6a7723756cca17458192a049edcf1b5b28
bo_chain <53370227+bochainwu@users.noreply.github.com>
2022-12-26 23:21:23 +0800

    Update final.ino
    add water sensor

--- final.ino
+++ final.ino
@@ -5,6 +5,7 @@ void setup() {
   byte numDigits = 4;
   byte digitPins[] = {10,11,12,13};
   byte segmentPins[] = {9,2,3,5,6,8,7,4};
+  pinMode(A0,OUTPUT);
 
   bool resistorsOnSegments = true;
   //bool updateWithDelaysIn = true;
@@ -15,7 +16,8 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  long a =  random(1,1000);
+  //long a =  random(1,1000);
+  int a = analogRead(A0);
   for(int i=0;i<10000;i++){
     sevseg.setNumber(a, 0);
     sevseg.refreshDisplay();
